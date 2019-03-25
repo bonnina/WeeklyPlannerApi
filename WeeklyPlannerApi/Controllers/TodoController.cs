8435c95ce38d4176c05915e12b509c08ef2e3bca
bonnina <37539513+bonnina@users.noreply.github.com>
2019-03-25 07:16:11 +0200

    Update temp settings

---
+++ WeeklyPlannerApi/Controllers/TodoController.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WeeklyPlannerApi.Models;
+
+namespace WeeklyPlannerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+
+            if (_context.TodoItems.Count() == 0)
+            {
+                // Create a new TodoItem if collection is empty
+                _context.TodoItems.Add(new TodoItem { Name = "TestItem" });
+                _context.SaveChanges();
+            }
+        }
+    }
+}
--- WeeklyPlannerApi/Startup.cs
+++ WeeklyPlannerApi/Startup.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using WeeklyPlannerApi.Models;
 
 namespace WeeklyPlannerApi
 {
@@ -21,14 +16,15 @@ namespace WeeklyPlannerApi
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(opt =>
+                opt.UseInMemoryDatabase("TodoList"));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -37,7 +33,6 @@ namespace WeeklyPlannerApi
             }
             else
             {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
